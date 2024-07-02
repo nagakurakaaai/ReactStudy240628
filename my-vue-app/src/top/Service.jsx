71715c71764b99c8dbefa6469ea110a71d93dcfc
nagakurakai <nagakura@mediowl.co.jp>
2024-07-02 11:47:19 +0900

    サービス項目を共通コンポーネント化

--- my-vue-app/src/top/Service.jsx
+++ my-vue-app/src/top/Service.jsx
@@ -1,3 +1,4 @@
+import ServiceArticle from '/src/top/ServiceArticle.jsx'
 
 function Service() {
   return (
@@ -9,45 +10,9 @@ function Service() {
           </div>
           <div className="service__body">
             <div className="service-list">
-              <div className="service-list__item">
-                <figure className="service-list__img-wrapper">
-                  <span className="service-list__img material-icons-round">
-                    work
-                  </span>
-                </figure>
-                <div className="service-list__body">
-                  <div className="service-list__title">人材紹介業</div>
-                  <div className="service-list__text">
-                    <p>新規サイトの制作はもちろんサイトリニューアルやランディングページの制作も可能です。</p>
-                  </div>
-                </div>
-              </div>
-              <div className="service-list__item">
-                <figure className="service-list__img-wrapper">
-                  <span className="service-list__img material-icons-round">
-                    location_city
-                  </span>
-                </figure>
-                <div className="service-list__body">
-                  <div className="service-list__title">スクール事業</div>
-                  <div className="service-list__text">
-                    <p>オンラインでWeb制作を学べるスクールを運営しています。</p>
-                  </div>
-                </div>
-              </div>
-              <div className="service-list__item">
-                <figure className="service-list__img-wrapper">
-                  <span className="service-list__img material-icons-round">
-                    desktop_windows
-                  </span>
-                </figure>
-                <div className="service-list__body">
-                  <div className="service-list__title">Webメディア運営</div>
-                  <div className="service-list__text">
-                    <p>人事系メディアやWebデザイン関連のメディアなどを複数運営しています。</p>
-                  </div>
-                </div>
-              </div>
+              <ServiceArticle icon="work" title="人材紹介業" text="新規サイトの制作はもちろんサイトリニューアルやランディングページの制作も可能です。"/>
+              <ServiceArticle icon="location_city" title="スクール事業" text="オンラインでWeb制作を学べるスクールを運営しています。"/>
+              <ServiceArticle icon="desktop_windows" title="Webメディア運営" text="人事系メディアやWebデザイン関連のメディアなどを複数運営しています。"/>
             </div>
           </div>
         </div>
--- my-vue-app/src/top/ServiceArticle.jsx
+++ my-vue-app/src/top/ServiceArticle.jsx
@@ -1,10 +1,30 @@
+import PropTypes from 'prop-types';
 
-function ServiceArticle() {
+function ServiceArticle(props) {
+  const {icon, title, text} = props
   return (
     <>
-    仮です
+    <div className="service-list__item">
+      <figure className="service-list__img-wrapper">
+        <span className="service-list__img material-icons-round">
+          {icon}
+        </span>
+      </figure>
+      <div className="service-list__body">
+        <div className="service-list__title">{title}</div>
+        <div className="service-list__text">
+          <p>{text}</p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
+
+ServiceArticle.propTypes = {
+  icon: PropTypes.string,
+  title: PropTypes.string,
+  text: PropTypes.string,
+};
 
 export default ServiceArticle
